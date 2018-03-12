567903ea6d76f20010a49fabd8b38883fd0d43a0
Tiller <ctiller@proforma.com>
2018-03-12 13:41:48 -0400

    add in basic score system and name update system

--- public/main.js
+++ public/main.js
@@ -1,24 +1,67 @@
 const main = () => {
   // document.querySelector('h1').textContent += '?';
+  // Initialize the current scores.
+  let teamOneScore = 0;
+  let teamTwoScore = 0;
+
+  let teamOneName = "Team 1";
+  let teamTwoName = "Team 2";
+
+  // Selecting the score and name elements.
+  let teamOneScoreDisplay = document.querySelector('.team1-score');
+  let teamTwoScoreDisplay = document.querySelector('.team2-score');
+
+  let teamOneNameDisplay = document.querySelector('.team1-name');
+  let teamTwoNameDisplay = document.querySelector('.team2-name');
+
+  let currentNameUpdateOne = document.querySelector('.new-team1-name');
+  let currentNameUpdateTwo = document.querySelector('.new-team2-name');
+
+  // Selecting the name update buttons and adding event listeners.
+  teamOneNameUpdate = document.querySelector('.team1-update-button');
+  teamTwoNameUpdate = document.querySelector('.team2-update-button');
+
+  teamOneNameUpdate.addEventListener('click', (e) => {
+    e.preventDefault();
+    teamOneNameDisplay.textContent = currentNameUpdateOne.value;
+    console.log(currentNameUpdateOne.value);
+  });
+
+  teamTwoNameUpdate.addEventListener('click', (e) => {
+    e.preventDefault();
+    teamTwoNameDisplay.textContent = currentNameUpdateTwo.value;
+    console.log(currentNameUpdateTwo.value);
+  });
+
+  // Selecting the score buttons and adding event listeners
   document.querySelector('.add-team-1').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log("Clicked! :D! Add one! Team 1!");
+    teamOneScore++;
+    teamOneScoreDisplay.textContent = teamOneScore;
+    console.log(`Current score: ${teamOneScore}`);
   });
 
   document.querySelector('.subtract-team-1').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log("Clicked! :D! Subtract one! Team 1!");
+    teamOneScore--;
+    teamOneScoreDisplay.textContent = teamOneScore;
+    console.log(`Current score: ${teamOneScore}`);
   });
 
   document.querySelector('.add-team-2').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log("Clicked! :D! Add one! Team 2!");
+    teamTwoScore++;
+    teamTwoScoreDisplay.textContent = teamTwoScore;
+    console.log(`Current score: ${teamTwoScore}`);
   });
 
   document.querySelector('.subtract-team-2').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log("Clicked! :D! Subtract one! Team 2!");
+    teamTwoScore--;
+    teamTwoScoreDisplay.textContent = teamTwoScore;
+    console.log(`Current score: ${teamTwoScore}`);
   });
+
 }
 
 document.addEventListener('DOMContentLoaded', main);
