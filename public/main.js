117741e93620f07e0ef8996156400212b4dd606b
Tiller <ctiller@proforma.com>
2018-03-12 13:06:06 -0400

    Create basic html structure, create basic css styling, add click events to add buttons on team scores.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,24 @@
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  // document.querySelector('h1').textContent += '?';
+  document.querySelector('.add-team-1').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Clicked! :D! Add one! Team 1!");
+  });
+
+  document.querySelector('.subtract-team-1').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Clicked! :D! Subtract one! Team 1!");
+  });
+
+  document.querySelector('.add-team-2').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Clicked! :D! Add one! Team 2!");
+  });
+
+  document.querySelector('.subtract-team-2').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Clicked! :D! Subtract one! Team 2!");
+  });
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main);
